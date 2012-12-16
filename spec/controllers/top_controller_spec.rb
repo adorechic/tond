a95200af0dbfb956ee424746ba24f1856577631c
adorechic <adorechic@gmail.com>
2012-12-16 10:11:08 +0900

    Add login controller spec

--- spec/controllers/top_controller_spec.rb
+++ spec/controllers/top_controller_spec.rb
@@ -3,10 +3,17 @@ require 'spec_helper'
 
 describe TopController do
   describe '#index' do
-    before { login_user }
     subject { get :index }
 
-    it { should be_success }
-    it { should render_template 'index' }
+    context 'if user is login' do
+      before { login_user }
+
+      it { should be_success }
+      it { should render_template 'index' }
+    end
+
+    context 'if user is not login' do
+      it { should redirect_to new_user_session_path }
+    end
   end
 end

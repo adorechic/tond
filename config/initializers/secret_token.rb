f9ac7f836bd482f7666851727677d761d7828ff3
adorechic <adorechic@gmail.com>
2013-07-28 10:46:03 +0900

    Add secret_key_base option

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,6 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Tond::Application.config.secret_token = '2ad796df08ab9f5e5548027c593f6c0c1b02bbeb2e30862e13929e38dfda0456b40bac228fe65802cd2e537c5d5d7a5bdb9bfb6cb12b49757a2f889231a80153'
+
+## heroku config:add TOND_SECRET_KEY_BASE=XXX
+Tond::Application.config.secret_key_base = ENV['TOND_SECRET_KEY_BASE'] || 'tond_secret_key_base'
